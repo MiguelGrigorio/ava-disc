6bd2f57cd981c5668c4e22110d6bf0a7f7cad2d1
Miguel Grigorio <miguelgrigorio@hotmail.com>
2024-09-09 13:30:41 -0300

    Segurança de login

--- download-calendar.py
+++ download-calendar.py
@@ -1,29 +1,26 @@
 import twill 
 from twill.commands import *
+from dotenv import load_dotenv
 import os
 
+load_dotenv()
+username = os.getenv('USERNAME')
+password = os.getenv('PASSWORD')
+
 go("https://ava3.cefor.ifes.edu.br")
-showforms()
 
-fv("1", "username", "20222ENEL0100")
-fv("1", "password", "S6C8iotSRz.")
-
-showforms()
+fv("1", "username", username)
+fv("1", "password", password)
 
 submit()
 print("Login realizado com sucesso!")
+
+
 go("https://ava3.cefor.ifes.edu.br/calendar/export.php?")
-showforms()
 
-# Seleciona todos os eventos
 fv("2", "events[exportevents]", "all")
-
-# Seleciona a semana atual
 fv("2", "period[timeperiod]", "weeknow")
 
-# Aperta o botão de exportar
 submit('export', '2')
-print("Exportação realizada com sucesso!")
-
-# Salva o arquivo
 save_html("calendar.ics")
+print("Exportação realizada com sucesso!")
