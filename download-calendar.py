050292f7e2507f42e1c3cc422eee37927e782720
John Doe <johndoe@example.com>
2024-09-09 14:07:25 -0300

    calendar download

--- download-calendar.py
+++ download-calendar.py
@@ -4,17 +4,18 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-username = os.getenv('USERNAME')
+
+login = os.getenv('LOGIN')
 password = os.getenv('PASSWORD')
 
 go("https://ava3.cefor.ifes.edu.br")
 
-fv("1", "username", username)
+
+fv("1", "username", login)
 fv("1", "password", password)
 
 submit()
 print("Login realizado com sucesso!")
-
 
 go("https://ava3.cefor.ifes.edu.br/calendar/export.php?")
 
@@ -22,5 +23,6 @@ fv("2", "events[exportevents]", "all")
 fv("2", "period[timeperiod]", "weeknow")
 
 submit('export', '2')
+
 save_html("calendar.ics")
 print("Exportação realizada com sucesso!")
